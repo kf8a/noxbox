357c9164daaa002d5acc906754bb2d28c4a1f5c2
sven bohm <bohms@msu.edu>
2014-09-17 10:29:14 -0400

    check for more errors

--- noxbox.go
+++ noxbox.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/json"
 	zmq "github.com/pebbe/zmq4"
 	serial "github.com/tarm/goserial"
 	"io"
 	"log"
-	"math"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -26,64 +24,56 @@ type Message struct {
 	At   time.Time `json:"at"`
 }
 
-func (noxbox NOXBOX) Sample() string {
-
-	data := noxbox.read()
-	for data == "" {
-		data = noxbox.read()
-	}
-	nox := noxbox.parse(data)
-
-	message := Message{nox, noxbox.site, time.Now()}
-	json_message, err := json.Marshal(message)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(json_message)
-}
-
-func (noxbox NOXBOX) parse(data string) float64 {
-	r, _ := regexp.Compile(`\d+E-?\d+`)
-	no := r.FindString(data)
-	result, err := strconv.ParseFloat(no, 64)
-	if err != nil {
-		log.Print(err, data, no)
-		return math.NaN()
-	}
-	return result
-}
-
-func (noxbox NOXBOX) read() string {
+func (noxbox NOXBOX) Sample() (string, error) {
 	c := serial.Config{Name: noxbox.device, Baud: 9600}
 	port, err := serial.OpenPort(&c)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer port.Close()
+	data := noxbox.read(port)
+	for data == "" {
+		data = noxbox.read(port)
+	}
+	nox, err := noxbox.parse(data)
+	if err != nil {
+		return "", err
+	}
 
-	result := new(bytes.Buffer)
+	message := Message{nox, noxbox.site, time.Now()}
+	json_message, err := json.Marshal(message)
+	return string(json_message), err
+}
+func (noxbox NOXBOX) parse(data string) (float64, error) {
+	/* log.Print(data) */
+	r, _ := regexp.Compile(`\d+E-?\d+`)
+	no := r.FindString(data)
+	result, err := strconv.ParseFloat(no, 64)
+	if err != nil {
+		log.Print(err, data, no)
+		return 0, err
+	}
+	return result, nil
+}
+
+func (noxbox NOXBOX) read(port io.ReadWriteCloser) string {
 
 	query := noxbox.address + "no\r"
-	_, err = port.Write([]byte(query))
+	_, err := port.Write([]byte(query))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for !strings.Contains(result.String(), "\r") {
-		buffer := make([]byte, 1024)
-		n, err := port.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				log.Fatal(err)
-			}
+	reader := bufio.NewReader(port)
+	reply, err := reader.ReadBytes('\x0A')
+	if err != nil {
+		if err == io.EOF {
+			/* log.Print(err) */
+		} else {
+			log.Fatal(err)
 		}
-
-		result.Write(buffer[:n])
 	}
-	return result.String()
+	return string(reply)
 }
 
 func main() {
@@ -100,9 +90,11 @@ func main() {
 	socket.Bind("tcp://*:5557")
 
 	for {
-		sample := noxbox.Sample()
-		log.Print(sample)
-		socket.Send(sample, 0)
-		time.Sleep(10)
+		sample, _ := noxbox.Sample()
+		if err == nil {
+			log.Print(sample)
+			socket.Send(sample, 0)
+		}
+		time.Sleep(10 * time.Second)
 	}
 }
